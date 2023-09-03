617a7b9cfd01758de4b3e5d86f0af8b51b16005a
Gabriel Abubakar <gabrielogrima@gmail.com>
2023-09-03 18:52:13 +0100

    tailwind variable colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-cyan': 'hsl(185, 75%, 39%)',
+        'desaturated-blue': 'hsl(229, 23%, 23%)',
+        'dark-grayish-blue': 'hsl(227, 10%, 46%)',
+        'dark-gray': 'hsl(0, 0%, 59%)'
+      }
+    },
   },
   plugins: [],
 }
