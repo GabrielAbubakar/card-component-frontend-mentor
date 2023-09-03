a22df05ebc014c8a1798ec9c5b8b222770d6d936
Gabriel Abubakar <gabrielogrima@gmail.com>
2023-09-03 19:18:29 +0100

    basic card html setup without styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
+import ProfileImg from '../public/image-victor.jpg'
+
 
 const App = () => {
     return (
-        <main>
-
+        <main className="bg-dark-cyan min-w-[100vw] min-h-[100vh]">
+            we
+            <div>
+                <figure>
+                    <img src={ProfileImg} alt="profile-image" />
+                </figure>
+                <div>
+                    <h1>Victor Crest</h1>
+                    <h1>26</h1>
+                </div>
+                <p>London</p>
+                <div>
+                    <div>
+                        <p>80K</p>
+                        <p>Followers</p>
+                    </div>
+                    <div>
+                        <p>803K</p>
+                        <p>Likes</p>
+                    </div>
+                    <div>
+                        <p>1.4K</p>
+                        <p>Photos</p>
+                    </div>
+                </div>
+            </div>
         </main>
     )
 }
 
-export default App
+export default App;
